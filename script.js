d2cda77ec3acb8e0567f96b4d85e5225449bf8c8
Rushie12 <124557624+Rushie12@users.noreply.github.com>
2023-06-02 11:13:46 +0700

    first commit

--- script.js
+++ script.js
@@ -11,4 +11,78 @@ document.addEventListener('click', (event) => {
         navbar.classList.remove('active');
     }
 });
+/* form validation */
 
+const form = document.getElementById('registration-form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const phoneInput = document.getElementById('phone');
+const addressInput = document.getElementById('address');
+
+function showError(input, message) {
+  const errorSpan = document.getElementById(`${input.id}-error`);
+  errorSpan.textContent = message;
+  errorSpan.style.display = 'block';
+  input.classList.add('error');
+}
+
+function hideError(input) {
+  const errorSpan = document.getElementById(`${input.id}-error`);
+  errorSpan.textContent = '';
+  errorSpan.style.display = 'none';
+  input.classList.remove('error');
+}
+
+function validateName() {
+  const name = nameInput.value.trim();
+  if (name === '') {
+    showError(nameInput, 'Name is required');
+  } else {
+    hideError(nameInput);
+  }
+}
+
+function validateEmail() {
+  const email = emailInput.value.trim();
+  if (email === '') {
+    showError(emailInput, 'Email is required');
+  } else if (!/\S+@\S+\.\S+/.test(email)) {
+    showError(emailInput, 'Email is not valid');
+  } else {
+    hideError(emailInput);
+  }
+}
+
+function validatePhone() {
+  const phone = phoneInput.value.trim();
+  if (phone === '') {
+    showError(phoneInput, 'Phone number is required');
+  } else if (!/^\d{10}$/.test(phone)) {
+    showError(phoneInput, 'Phone number must be a 10-digit number');
+  } else {
+    hideError(phoneInput);
+  }
+}
+
+function validateAddress() {
+  const address = addressInput.value.trim();
+  if (address === '') {
+    showError(addressInput, 'Address is required');
+  } else {
+    hideError(addressInput);
+  }
+}
+
+function validateForm(event) {
+  event.preventDefault();
+  validateName();
+  validateEmail();
+  validatePhone();
+  validateAddress();
+}
+
+form.addEventListener('submit', validateForm);
+nameInput.addEventListener('blur', validateName);
+emailInput.addEventListener('blur', validateEmail);
+phoneInput.addEventListener('blur', validatePhone);
+addressInput.addEventListener('blur', validateAddress);
